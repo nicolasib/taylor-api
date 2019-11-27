2517552d31b0ce8b211f4d7146f76f15e9bf59ba
nicolasib <design.nicolasib@gmail.com>
2019-11-27 12:30:32 -0300

    bug fix

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,4 @@ const  AlbumController = require('./Controllers/AlbumController')
 
 routes.post('/registerAlbum', AlbumController.store)
 routes.post('/listAlbuns', AlbumController.index)
-
 module.exports = routes
